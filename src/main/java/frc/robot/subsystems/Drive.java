b652060c61590e03c987f9e8178474e1e356062f
mayarouth <routhmaya@gmail.com>
2020-02-19 16:14:50 -0600

    created constructor

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.Constants;
 
 public class Drive extends SubsystemBase {
 
@@ -10,5 +12,16 @@ public class Drive extends SubsystemBase {
     public static CANSparkMax driveControllerRight;
 
     public static DifferentialDrive drivetrain;
+
+    public Drive(){
+        driveControllerLeft = new CANSparkMax(Constants.ID_SPARKMAX_DRIVE_LEFT, MotorType.kBrushed);
+        driveControllerRight = new CANSparkMax(Constants.ID_SPARKMAX_DRIVE_RIGHT, MotorType.kBrushed);
+
+        driveControllerLeft.setInverted(false);
+        driveControllerRight.setInverted(true);
+
+        drivetrain = new DifferentialDrive(driveControllerLeft, driveControllerRight);
+
+    }
 
 }
