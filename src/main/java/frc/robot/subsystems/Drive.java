8990a13fd1f00e124983f7e4441eceff2f313a9e
mayarouth <routhmaya@gmail.com>
2020-02-19 15:47:08 -0600

    drive subsystem variables declared

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -3,3 +3,12 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.revrobotics.CANSparkMax;
+
+public class Drive extends SubsystemBase {
+
+    public static CANSparkMax driveControllerLeft;
+    public static CANSparkMax driveControllerRight;
+
+    public static DifferentialDrive drivetrain;
+
+}
