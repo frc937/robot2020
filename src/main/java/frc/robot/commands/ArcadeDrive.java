e5014cff2725bbd9ad51f35b4e8ee9729df7c912
mayarouth <routhmaya@gmail.com>
2020-02-19 19:33:08 -0600

    created command skeleton

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -2,3 +2,31 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drive;
+
+public class ArcadeDrive extends CommandBase {
+
+    public ArcadeDrive(){
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+}
