63698923c547ec1ffbcb4eec2de70e44f1b0df9a
mayarouth <routhmaya@gmail.com>
2020-02-19 19:51:24 -0600

    puts drive mode on dash on initialize

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drive;
 
@@ -15,11 +16,13 @@ public class ArcadeDrive extends CommandBase {
 
     @Override
     public void initialize() {
+        SmartDashboard.putString("Drive Mode", "Arcade");
 
     }
 
     @Override
     public void execute() {
+        
 
     }
 
