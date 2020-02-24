81852994f7984aa4e219ca0778789550bd9f6a68
fluffhead921 <routhmaya@gmail.com>
2020-02-24 16:41:46 -0600

    created camera subsystem

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -1,5 +1,20 @@
 package frc.robot.subsystems;
 
-public class Camera {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.cameraserver.CameraServer;
+
+public class Camera extends SubsystemBase{
+
+    private int device;
+
+    public Camera(int port) {
+        device = port;
+
+    }
+
+    public void startCamera() {
+        CameraServer.getInstance().startAutomaticCapture(device);
+
+    }
+
 }
