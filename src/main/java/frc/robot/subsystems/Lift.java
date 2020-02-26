d1d27133c46b279bc7a85608b665d364fe6e7138
fluffhead921 <routhmaya@gmail.com>
2020-02-26 15:31:21 -0600

    declared a lift controller

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
 
 public class Lift extends SubsystemBase {
 
-    
+    private CANSparkMax liftController;
+
 }
