ce9743394655de779a2838bfbd96adb9660e7e1b
fluffhead921 <routhmaya@gmail.com>
2020-02-26 15:43:43 -0600

    added requirements for CANSparkMax

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.Constants;
 
 public class Lift extends SubsystemBase {
 
@@ -9,7 +11,7 @@ public class Lift extends SubsystemBase {
 
     public Lift() {
 
-        liftController = new CANSparkMax(deviceID, type);
+        liftController = new CANSparkMax(Constants.ID_SPARKMAX_LIFT, MotorType.kBrushed);
 
     }
 
