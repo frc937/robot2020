cf88529c2c7f78d559ec45a81cdfde396d9d375a
fluffhead921 <routhmaya@gmail.com>
2020-02-27 16:59:52 -0600

    declared variables

--- src/main/java/frc/robot/custom_buttons/TwoButtonCombo.java
+++ src/main/java/frc/robot/custom_buttons/TwoButtonCombo.java
@@ -1,7 +1,12 @@
 package frc.robot.custom_buttons;
 
 import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj.GenericHID;
 
 public class TwoButtonCombo extends Button {
+
+    private GenericHID controller;
+    private int button1Number;
+    private int button2Number;
 
 }
