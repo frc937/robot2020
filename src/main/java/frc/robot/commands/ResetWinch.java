635b281bedfa053865d679fa3d75c9c20294021b
fluffhead921 <routhmaya@gmail.com>
2020-02-27 16:12:42 -0600

    declared lift subsystem

--- src/main/java/frc/robot/commands/ResetWinch.java
+++ src/main/java/frc/robot/commands/ResetWinch.java
@@ -1,8 +1,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Lift;
 
 public class ResetWinch extends CommandBase {
 
-    
+    private Lift liftWinch;
+
 }
