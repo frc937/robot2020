dee42a5317ae2588e48e00445c514033a042c4aa
fluffhead921 <routhmaya@gmail.com>
2020-03-03 17:40:45 -0600

    declared a victorSPX

--- src/main/java/frc/robot/subsystems/BallIntake.java
+++ src/main/java/frc/robot/subsystems/BallIntake.java
@@ -1,12 +1,15 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class BallIntake extends SubsystemBase {
 
+    private VictorSPX intakeController;
+
     public BallIntake() {
 
-        
+
 
     }
 
