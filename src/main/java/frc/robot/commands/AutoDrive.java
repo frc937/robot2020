d8d2daff23f2a422c582aa99ebb1f7dec744e11c
fluffhead921 <routhmaya@gmail.com>
2020-03-10 16:19:41 -0500

    initialize puts "Autonomous" in "Drive Mode" on dash

--- src/main/java/frc/robot/commands/AutoDrive.java
+++ src/main/java/frc/robot/commands/AutoDrive.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drive;
 
@@ -17,6 +18,7 @@ public class AutoDrive extends CommandBase {
 
     @Override
     public void initialize() {
+        SmartDashboard.putString("Drive Mode", "Autonomous");
         
     }
 
