72778cdc22fe2c6f32b88a8cba0c58c41583537c
fluffhead921 <routhmaya@gmail.com>
2020-03-10 15:10:59 -0500

    changed lift speeds to 30%

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -23,7 +23,7 @@ public class Lift extends SubsystemBase {
 
     public void turn() {
 
-        liftSpeed = 1.0;
+        liftSpeed = 0.3;
         liftController1.set(liftSpeed);
         liftController2.set(liftSpeed);
 
@@ -39,7 +39,7 @@ public class Lift extends SubsystemBase {
 
     public void reset() {
 
-        liftSpeed = -1.0;
+        liftSpeed = -0.3;
         liftController1.set(liftSpeed);
         liftController2.set(liftSpeed);
 
