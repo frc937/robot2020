0fa47c6deaf3d9e4b72782c07b0ec2bf16fb106d
fluffhead921 <routhmaya@gmail.com>
2020-03-10 16:51:51 -0500

    changed to parallel race group (all end when one ends)

--- src/main/java/frc/robot/commands/groups/AutoGroup.java
+++ src/main/java/frc/robot/commands/groups/AutoGroup.java
@@ -1,11 +1,11 @@
 package frc.robot.commands.groups;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Drive;
 import frc.robot.commands.AutoDrive;
 
-public class AutoGroup extends ParallelCommandGroup {
+public class AutoGroup extends ParallelRaceGroup {
 
     public AutoGroup(Drive drivetrain) {
         addCommands(
