1640989de0e653f177afe758786a28ca592c4837
fluffhead921 <routhmaya@gmail.com>
2020-03-10 16:45:26 -0500

    imported my AutoDrive and frc's WaitCommand

--- src/main/java/frc/robot/commands/groups/AutoGroup.java
+++ src/main/java/frc/robot/commands/groups/AutoGroup.java
@@ -1,12 +1,14 @@
 package frc.robot.commands.groups;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Drive;
+import frc.robot.commands.AutoDrive;
 
 public class AutoGroup extends ParallelCommandGroup {
 
     public AutoGroup(Drive drivetrain) {
-        
+
 
     }
 
