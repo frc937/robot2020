2fdac6c33aaff064fb981e91efa0cfbb357aa9ff
fluffhead921 <routhmaya@gmail.com>
2020-03-10 16:43:06 -0500

    created constructor with required subsystem

--- src/main/java/frc/robot/commands/groups/AutoGroup.java
+++ src/main/java/frc/robot/commands/groups/AutoGroup.java
@@ -1,7 +1,13 @@
 package frc.robot.commands.groups;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.Drive;
 
 public class AutoGroup extends ParallelCommandGroup {
-    
+
+    public AutoGroup(Drive drivetrain) {
+        
+
+    }
+
 }
