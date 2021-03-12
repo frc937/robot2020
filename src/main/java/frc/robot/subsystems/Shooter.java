b7880c0c3238d79656215805a424c99c02a23675
fluffhead921 <routhmaya@gmail.com>
2021-03-12 17:39:26 -0600

    Created go and stop methods for shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,13 +4,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import frc.robot.Constants;
+import frc.robot.commands.GetShooterSpeed;
 
 public class Shooter extends SubsystemBase {
 
     public VictorSPX shoot;
+    public GetShooterSpeed shootSpeed;
+
 
     public Shooter() {
         shoot = new VictorSPX(Constants.ID_VICTORSPX_SHOOTER);
+
+    }
+
+    public void go() {
+        shoot.set(ControlMode.PercentOutput, shootSpeed.speed);
+
+    }
+
+    public void stop() {
+        shoot.set(ControlMode.PercentOutput, 0.0);
 
     }
 
