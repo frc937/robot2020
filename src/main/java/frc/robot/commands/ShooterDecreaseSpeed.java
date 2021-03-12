6bffaff3780ba4d26688691974bd2fefc711cc6f
fluffhead921 <routhmaya@gmail.com>
2021-03-12 17:32:33 -0600

    Completed global shooter speed incrementation

--- src/main/java/frc/robot/commands/ShooterDecreaseSpeed.java
+++ src/main/java/frc/robot/commands/ShooterDecreaseSpeed.java
@@ -5,9 +5,33 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class ShooterDecreaseSpeed extends CommandBase {
 
     public double speed;
+    public GetShooterSpeed getShooterSpeed;
 
     public ShooterDecreaseSpeed() {
+        getShooterSpeed = new GetShooterSpeed(false);
 
     }
+
+    @Override
+    public void initialize() {
+        speed = getShooterSpeed.speed;
+        speed = speed + 0.05;
+    }
+  
+    @Override
+    public void execute() {
+    }
+ 
+    @Override
+    public void end(boolean interrupted) {
+        getShooterSpeed.schedule();
+
+    }
+  
+    @Override
+    public boolean isFinished() {
+      return false;
+    }
+    
 
 }
--- src/main/java/frc/robot/commands/ShooterIncreaseSpeed.java
+++ src/main/java/frc/robot/commands/ShooterIncreaseSpeed.java
@@ -1,13 +1,37 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.commands.GetShooterSpeed;
 
 public class ShooterIncreaseSpeed extends CommandBase {
 
     public double speed;
+    public GetShooterSpeed getShooterSpeed;
 
     public ShooterIncreaseSpeed() {
+        getShooterSpeed = new GetShooterSpeed(true);
 
+    }
+
+    @Override
+    public void initialize() {
+        speed = getShooterSpeed.speed;
+        speed = speed + 0.05;
+    }
+  
+    @Override
+    public void execute() {
+    }
+ 
+    @Override
+    public void end(boolean interrupted) {
+        getShooterSpeed.schedule();
+
+    }
+  
+    @Override
+    public boolean isFinished() {
+      return false;
     }
     
 }
