472fb27dd42894f3fc12105b5710de1310fff336
fluffhead921 <routhmaya@gmail.com>
2021-03-12 16:07:43 -0600

    Added shoot variable

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,11 +1,17 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
 
-    
+    public VictorSPX shoot;
+
+    public Shooter() {
+        shoot = new VictorSPX(Constants.ID_VICTORSPX_SHOOTER);
+
+    }
+
 }
