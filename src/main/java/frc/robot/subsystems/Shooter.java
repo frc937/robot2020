6faea9a5ff28d13eb04cc15d60c5f5e7f73bd4c5
fluffhead921 <routhmaya@gmail.com>
2021-03-12 15:37:53 -0600

    Made it a class

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,3 +4,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants;
+
+public class Shooter extends SubsystemBase {
+
+    
+}
