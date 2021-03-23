1f0e977488d616cf181b42790597c32d5d873cd1
fluffhead921 <routhmaya@gmail.com>
2021-03-23 17:17:36 -0500

    Created skeleton for PID Drive system

--- src/main/java/frc/robot/subsystems/PIDDrive.java
+++ src/main/java/frc/robot/subsystems/PIDDrive.java
@@ -1,1 +1,18 @@
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+
+public class PIDDrive extends PIDSubsystem { 
+
+    public PIDDrive() {
+
+    }
+
+    public double returnPIDInput() {
+
+    }
+
+    public void usePIDOutput() {
+        
+    }
+
+}
