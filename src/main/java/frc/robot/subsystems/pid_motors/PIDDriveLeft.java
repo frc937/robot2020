dea2bb515a522cfac59701631b321201dcdf7d16
fluffhead921 <routhmaya@gmail.com>
2021-03-24 16:36:13 -0500

    Removed right side motor controller and encoder

--- src/main/java/frc/robot/subsystems/pid_motors/PIDDriveLeft.java
+++ src/main/java/frc/robot/subsystems/pid_motors/PIDDriveLeft.java
@@ -8,9 +8,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class PIDDriveLeft extends PIDSubsystem { 
 
-    private CANSparkMax driveControllerRight;
     private CANSparkMax driveControllerLeft;
-    private Encoder driveEncoderRight;
     private Encoder driveEncoderLeft;
 
     private static PIDController pidController;
@@ -21,12 +19,9 @@ public class PIDDriveLeft extends PIDSubsystem {
         pidController = new PIDController(0, 0, 0);
 
         driveControllerLeft = new CANSparkMax(Constants.ID_SPARKMAX_DRIVE_LEFT, MotorType.kBrushed);
-        driveControllerRight = new CANSparkMax(Constants.ID_SPARKMAX_DRIVE_RIGHT, MotorType.kBrushed);
         driveEncoderLeft = new Encoder(Constants.PORT_ENCODER_DRIVE_LEFT_1, Constants.PORT_ENCODER_DRIVE_LEFT_2);
-        driveEncoderRight = new Encoder(Constants.PORT_ENCODER_DRIVE_RIGHT_1, Constants.PORT_ENCODER_DRIVE_RIGHT_2);
 
         driveEncoderLeft.setDistancePerPulse(1.97119);
-        driveEncoderRight.setDistancePerPulse(1.97119);
 
     }
 
