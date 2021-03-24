2b4e683b64eed66668343dfd529822829ee3f02e
fluffhead921 <routhmaya@gmail.com>
2021-03-24 15:47:32 -0500

    created motor controllers and encoders, updated skeleton

--- src/main/java/frc/robot/subsystems/PIDDrive.java
+++ src/main/java/frc/robot/subsystems/PIDDrive.java
@@ -1,18 +1,39 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+import frc.robot.Constants;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class PIDDrive extends PIDSubsystem { 
 
+    private CANSparkMax driveControllerRight;
+    private CANSparkMax driveControllerLeft;
+    private Encoder driveEncoderRight;
+    private Encoder driveEncoderLeft;
+
+    private static PIDController pidController;
+
     public PIDDrive() {
+        super(pidController);
+
+        pidController = new PIDController(0, 0, 0);
+
+        driveControllerLeft = new CANSparkMax(Constants.ID_SPARKMAX_DRIVE_LEFT, MotorType.kBrushed);
+        driveControllerRight = new CANSparkMax(Constants.ID_SPARKMAX_DRIVE_RIGHT, MotorType.kBrushed);
+        driveEncoderLeft = new Encoder(sourceA, sourceB);
+        driveEncoderRight = new Encoder(sourceA, sourceB);
 
     }
 
-    public double returnPIDInput() {
+    public double getMeasurements() {
+
 
     }
 
-    public void usePIDOutput() {
-        
+    public void useOutput(double output, double setpoint) {
+
     }
 
 }
