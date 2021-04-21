bd94e007710fb67a8b1e2d1d5837c994c4d0cffd
fluffhead921 <routhmaya@gmail.com>
2021-04-21 15:23:34 -0500

    changed PIDController to ProfiledPIDController, changed raw p, i, and d values to variables

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -1,8 +1,13 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+//import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.Encoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants;
@@ -14,8 +19,15 @@ public class Drive extends SubsystemBase {
 
     public static CANSparkMax driveControllerLeft;
     public static CANSparkMax driveControllerRight;
-
+    public static Encoder driveEncoderRight;
+    public static Encoder driveEncoderLeft;
+    public static ProfiledPIDController pidController;
     public static DifferentialDrive drivetrain;
+    private double pValue;
+    private double iValue;
+    private double dValue;
+    private double maxVelocity;
+    private double maxAcceleration;
 
     /*
     public double leftXValue = controller.getX(Hand.kLeft);
@@ -26,13 +38,19 @@ public class Drive extends SubsystemBase {
 
     public Drive(){
 
+        pidController = new ProfiledPIDController(pValue, iValue, dValue, new TrapezoidProfile.Constraints(maxVelocity, maxAcceleration));
+
         driveControllerLeft = new CANSparkMax(Constants.ID_SPARKMAX_DRIVE_LEFT, MotorType.kBrushed);
         driveControllerRight = new CANSparkMax(Constants.ID_SPARKMAX_DRIVE_RIGHT, MotorType.kBrushed);
 
-        driveControllerLeft.setInverted(false);
-        driveControllerRight.setInverted(true);
+        driveEncoderLeft = new Encoder(Constants.PORT_ENCODER_DRIVE_LEFT_1, Constants.PORT_ENCODER_DRIVE_LEFT_2);
 
         drivetrain = new DifferentialDrive(driveControllerLeft, driveControllerRight);
+
+        driveEncoderLeft.setDistancePerPulse(1.97119);
+
+        driveControllerLeft.setInverted(false);
+        driveControllerRight.setInverted(true);
 
     }
 
