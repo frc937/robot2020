259ddb9e05e0347a2b14522e411f37e43d3d5fb6
fluffhead921 <routhmaya@gmail.com>
2021-05-10 16:46:27 -0500

    Added feedforward + pid control to go to a set position (goToPosition) and moveTank

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -5,8 +5,9 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Encoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -22,12 +23,17 @@ public class Drive extends SubsystemBase {
     public static Encoder driveEncoderRight;
     public static Encoder driveEncoderLeft;
     public static ProfiledPIDController pidController;
+    public static SimpleMotorFeedforward feedforward;
     public static DifferentialDrive drivetrain;
+
     private double pValue;
     private double iValue;
     private double dValue;
     private double maxVelocity;
     private double maxAcceleration;
+
+    private double lastSpeed;
+    private double lastTime;
 
     /*
     public double leftXValue = controller.getX(Hand.kLeft);
@@ -50,23 +56,37 @@ public class Drive extends SubsystemBase {
         driveControllerRight = new CANSparkMax(Constants.ID_SPARKMAX_DRIVE_RIGHT, MotorType.kBrushed);
 
         driveEncoderLeft = new Encoder(Constants.PORT_ENCODER_DRIVE_LEFT_1, Constants.PORT_ENCODER_DRIVE_LEFT_2);
+        driveEncoderRight = new Encoder(Constants.PORT_ENCODER_DRIVE_RIGHT_1, Constants.PORT_ENCODER_DRIVE_RIGHT_2);
 
         drivetrain = new DifferentialDrive(driveControllerLeft, driveControllerRight);
 
         driveEncoderLeft.setDistancePerPulse(1.97119);
+        // Needs a driveEncoderRight
 
         driveControllerLeft.setInverted(false);
         driveControllerRight.setInverted(true);
 
+        driveEncoderLeft.setReverseDirection(false);
+        driveEncoderRight.setReverseDirection(true);
+
     }
 
+    /* Should add FeedForward and PID with gyro sensor once NavX oporational */
     public void moveArcade(double x, double z){
+
+        //double ffpidX = feedforward.calculate(x) + pidController.calculate(driveEncoderLeft.getRate(), x);
+        //double ffpidZ = feedforward.calculate(z) + pidController.calculate(driveEncoderRight.getRate(), z);
+
         drivetrain.arcadeDrive(x, z, true);
 
     } 
 
     public void moveTank(double lSpeed, double rSpeed){
-        drivetrain.tankDrive(lSpeed, rSpeed, true);
+
+        double ffpidLSpeed = feedforward.calculate(lSpeed) + pidController.calculate(driveEncoderLeft.getRate(), lSpeed);
+        double ffpidRSpeed = feedforward.calculate(rSpeed) + pidController.calculate(driveEncoderRight.getRate(), rSpeed);
+
+        drivetrain.tankDrive(ffpidLSpeed, ffpidRSpeed, true);
 
     }
 
@@ -76,4 +96,15 @@ public class Drive extends SubsystemBase {
         
     }
 
+    public void goToPosition(double goalPosition){
+        double acceleration = (pidController.getSetpoint().velocity - lastSpeed) / (Timer.getFPGATimestamp() - lastTime);
+        driveControllerLeft.setVoltage(pidController.calculate(driveEncoderLeft.getDistance(), 
+            goalPosition) + feedforward.calculate(pidController.getSetpoint().velocity, 
+            acceleration));
+        lastSpeed = pidController.getSetpoint().velocity;
+        lastTime = Timer.getFPGATimestamp();
+
+    }
+
+    
 }
