d9bea7f24ff9ddfe06cef01a1c6be2aed9375f8b
fluffhead921 <routhmaya@gmail.com>
2021-05-12 15:50:21 -0500

    Turned LinearFacingPositionDrive file into class
    -Braden |  There once was a man named Tim.

--- src/main/java/frc/robot/commands/LinearFacingPositionDrive.java
+++ src/main/java/frc/robot/commands/LinearFacingPositionDrive.java
@@ -4,5 +4,10 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+
+public class LinearFacingPositionDrive extends CommandBase {
+
+}
