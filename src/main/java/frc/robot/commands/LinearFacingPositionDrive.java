b432482bbbca6b8be65fe56a41055ce3b9e3338b
fluffhead921 <routhmaya@gmail.com>
2021-05-12 15:59:28 -0500

    Added LinearFacingPositionDrive skeleton
    -Braden | He had a normal life, as he was only a farm boy.

--- src/main/java/frc/robot/commands/LinearFacingPositionDrive.java
+++ src/main/java/frc/robot/commands/LinearFacingPositionDrive.java
@@ -1,4 +1,5 @@
-/** Yes, this command has an amazing, long, NON redundant name. I tried to convince him otherwise, 
+/** Command by Mr. Braden Reighard the VI & and amazing
+ * Yes, this command has an amazing, long, NON redundant name. I tried to convince him otherwise, 
  * but my villanous efforts were ingnored. Now this class has a beautiful non-icky name. Thanks to Braden. >:{}
  */
 
@@ -9,5 +10,29 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class LinearFacingPositionDrive extends CommandBase {
+    
+    @Override
+    public void initialize() {
+        //Put stuff here
+    }
+
+    @Override
+    public void execute() {
+        //Put more stuff here
+    }
+
+    @Override
+    public void end(boolean Interrupted) {
+        //Put even more STUFF here
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+        //Put Chuck Norris here
+    }
+
+
+
 
 }
