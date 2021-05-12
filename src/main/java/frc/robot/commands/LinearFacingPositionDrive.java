d3a0e3cfbb3c97e0233d2fce1fb0bbe93d6b16c8
fluffhead921 <routhmaya@gmail.com>
2021-05-12 15:45:52 -0500

    Created LinearFacingPositionDrive file.
    -Braden

---
+++ src/main/java/frc/robot/commands/LinearFacingPositionDrive.java
@@ -0,0 +1,8 @@
+/** Yes, this command has an amazing, long, NON redundant name. I tried to convince him otherwise, 
+ * but my villanous efforts were ingnored. Now this class has a beautiful non-icky name. Thanks to Braden. >:{}
+ */
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
